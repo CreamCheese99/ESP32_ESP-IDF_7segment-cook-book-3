bd8aa635a14e37446efe3558a966c2878fa85f6e
CreamCheese099 <thanchira50045@gmal.com>
2024-10-30 13:34:13 +0700

    cook-book-3

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include "SevenSegment.h"
 
-SevenSegment s1(0) ;
-SevenSegment s2(4) ;
+SevenSegment s1(0);  // จอแสดงผลตัวที่ 1
+SevenSegment s2(4);  // จอแสดงผลตัวที่ 2
 
 extern "C" void app_main(void)
 {
-    while(1)
+    while(1)   
     {
+        // ทดสอบฮาร์ดแวร์ s1 และแสดงหน้าจอว่างเปล่า
         s1.HardwareTest();
-        vTaskDelay(500/portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
         s1.DisplayBlank();
-        vTaskDelay(500/portTICK_PERIOD_MS);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        // ทดสอบฮาร์ดแวร์ s2 และแสดงหน้าจอว่างเปล่า
+        s2.HardwareTest();
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        s2.DisplayBlank();
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     } 
 }
